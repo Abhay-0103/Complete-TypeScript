78bddf5019eba302d51644663df6840ff3ddac5d
Abhay-0103 <ab0321054@gmail.com>
2025-12-24 22:32:10 +0530

    delted

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello, ${person}, Welcome to TypeScript!`;
-}
-const username = "Abhay Singh";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
