774b4ff9a9e8827d2cf80c87ec5de24e216a1086
Abhay-0103 <ab0321054@gmail.com>
2025-12-24 22:40:54 +0530

    Types Annotations and Impressions

---
+++ src/01_typesInTypes.ts
@@ -0,0 +1,11 @@
+let drink = "Coffee"
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let singer_Is = "Abhay Singh"
+
+let Abhay_Singing : string= "Rommantic Song"
+Abhay_Singing = "Emotional Song"
+
+
+let Say_To_Sing : boolean
