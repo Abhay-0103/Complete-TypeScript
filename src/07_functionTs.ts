d0450e8ff69d863c58ede9022c4408c2b6bf3a32
Abhay-0103 <ab0321054@gmail.com>
2026-01-23 23:40:33 +0530

    Add functions for chai preparation, pricing, and order management

---
+++ src/07_functionTs.ts
@@ -0,0 +1,34 @@
+function makeChai(type: string, cups: number) {
+    console.log(`Making ${cups} cups of {type}`);
+}
+
+makeChai("Masala", 2);
+
+function getChaiPrice(): number {
+    return 25;
+}
+
+function makeOrder(order: string) {
+    if (!order) return null;
+}
+
+function logBhai(): void {
+    console.log("Bhai is ready");
+}
+
+// function orderLadki(type?: string) {
+
+// }
+
+function orderLadki(type: string = "Mast Item") {
+    console.log(`Ordering a ladki of type: ${type}`);
+}
+
+function createLadki(order: {
+    name: string;
+    age: number;
+    isHot: boolean;
+}) {
+    console.log("Creating ladki with order:", order);
+}
+   
