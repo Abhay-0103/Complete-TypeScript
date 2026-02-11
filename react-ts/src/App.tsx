164846f7edee3291833e778f489ba527afffead9
Abhay-0103 <ab0321054@gmail.com>
2026-02-11 14:26:34 +0530

    Added Card and count value

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ChaiCard } from "./components/ChaiCard.tsx";
+import { Counter } from "./components/Counter.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello Bhai</h1>
+        <ChaiCard name="Adrak Chai" price="₹20" />
+        <ChaiCard name="Elaichi Chai" price="₹25" isSpecial />
+        <div>
+          <Counter />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
