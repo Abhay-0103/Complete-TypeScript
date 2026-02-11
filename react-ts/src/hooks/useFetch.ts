09313630b910f9f069ef78242f47ba80327fb2d6
Abhay-0103 <ab0321054@gmail.com>
2026-02-11 15:17:05 +0530

    Compeleteddd

---
+++ react-ts/src/hooks/useFetch.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+interface fetchState<T> {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+}
+
+export function useFetch<T>(url:string): fetchState<T> {
+    const [state, setState] = useState<fetchState<T>>({
+        data: null,
+    loading: true,
+    error: null,
+    });
+
+    // useEffect to make fectch request
+
+
+
+
+
+    return state;
+}
