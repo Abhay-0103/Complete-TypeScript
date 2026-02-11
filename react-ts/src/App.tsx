9216b7ee912b1be28786539fe7209acb6c31bb51
Abhay-0103 <ab0321054@gmail.com>
2026-02-11 14:59:41 +0530

    add ChaiList and OrderForm components to App for chai menu display and order submission

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,16 @@
 import "./App.css";
 import { ChaiCard } from "./components/ChaiCard.tsx";
+import ChaiList from "./components/ChaiList.tsx";
 import { Counter } from "./components/Counter.tsx";
+import { OrderForm } from "./components/OrderForm.tsx";
+
+import type { Chai } from "./types.ts";
+
+const menu: Chai[] = [
+  { id: 1, name: "Masala Chai", price: 20, isHot: true },
+  { id: 2, name: "Adrak Chai", price: 25 },
+  { id: 3, name: "Elaichi Chai", price: 30, isHot: true },
+];
 
 function App() {
   return (
@@ -11,6 +21,16 @@ function App() {
         <ChaiCard name="Elaichi Chai" price="₹25" isSpecial />
         <div>
           <Counter />
+        </div>
+        <div>
+          <ChaiList items={menu} />
+        </div>
+        <div>
+          <OrderForm
+            onSubmit={(order) => {
+              console.log("Order received", order.name, order.cups);
+            }}
+          />
         </div>
       </div>
     </>
