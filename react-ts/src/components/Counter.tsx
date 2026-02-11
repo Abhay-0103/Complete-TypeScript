6f5ab675c0819d5b5c4684b0c9b35052c4a9f288
Abhay-0103 <ab0321054@gmail.com>
2026-02-11 14:26:18 +0530

    Added Count value

---
+++ react-ts/src/components/Counter.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Cups of tea Ordered: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Order One More</button>
+    </div>
+  );
+}
